3b7f697f0246d8302ee6d7781cf4cdc36610223d
Felpinhon <lfelipe.lf990@gmmail.com>
2023-04-14 02:09:49 -0300

    index 99% completa

--- java.js
+++ java.js
@@ -21,29 +21,29 @@ function expandirPaulo() {
         divPaulo.style.border = '3px solid #000'
         divPaulo.style.width = '25.4%'
         divPaulo.style.left = '25%'
-    
+
         divDiscord.style.width = '25.5%'
         divDiscord.style.left = '-0.5%'
-    
+
         imgDiscord.style.width = '68%'
-    
+
         divJoao.style.width = '23.5%'
         divJoao.style.left = '8.5%'
         divJoao.style.flex = '0%'
-    
+
         divLuis.style.width = '25.5%'
         divLuis.style.left = '17.1%'
-    
+
         imgLuis.style.width = '135%'
-    
+
         divPaulo.classList.add('expandir-paulo')
-    
+
         setTimeout(() => {
-    
+
             divPaulo.style.width = '100%'
             divPaulo.style.left = '0%'
             divPaulo.classList.remove('expandir-paulo')
-    
+
         }, 1000)
 
     }
